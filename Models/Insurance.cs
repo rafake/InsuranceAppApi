48fdc9be9abd5266a1ed76c1a4317578da7a4632
Rafał Kelm <rkelm@efigence.com>
2021-08-25 21:08:32 +0200

    Add project files.

---
+++ Models/Insurance.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsuranceAppApi.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Insurance : ControllerBase
+    {
+        public int InsuranceId { get; set; }
+        public string InsuranceName { get; set; }
+        public string InsuranceCategory { get; set; }
+        public string InsuranceCompany { get; set; }
+        public string DateOfJoining { get; set; }
+        public string DateOfEnd { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+}
